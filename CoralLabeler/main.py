ee11bea0d586e9a415b7901e2a3714e2b5825712
Dylan Chapell <dylanchapell@rocketmail.com>
2024-01-26 18:47:48 -0700

    Add toolbox to QML context in main

--- CoralLabeler/main.py
+++ CoralLabeler/main.py
@@ -8,15 +8,18 @@ from PySide6.QtQml import QQmlApplicationEngine
 from PySide6.QtWidgets import QApplication, QPushButton
 from PySide6.QtCore import Slot
 
-
-
+from toolbox import Toolbox
 
 
 if __name__ == "__main__":
+    tbox = Toolbox()
+
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
     qml_file = Path(__file__).resolve().parent / "main.qml"
     engine.load(qml_file)
+    context = engine.rootContext()
+    context.setContextProperty("tbox",tbox)
     if not engine.rootObjects():
         sys.exit(-1)
     sys.exit(app.exec())
