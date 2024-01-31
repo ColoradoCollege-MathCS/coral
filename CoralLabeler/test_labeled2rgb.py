bcafa6ae3ca6cb23a6c6a8b7b186982d2032dc10
Dylan Chapell <dylanchapell@rocketmail.com>
2024-01-31 14:21:17 -0700

    Add revised ellipse to test file

--- CoralLabeler/test_labeled2rgb.py
+++ CoralLabeler/test_labeled2rgb.py
@@ -14,6 +14,25 @@ def circle_select(labels, labelNum, pointClicked, radius):
     #Fill in labelNum where true
     np.place(labels, in_circle, labelNum)
 
+def ellipse_select(labels, labelNum, point1, point2):
+    min_x = max(min(point1[0],point2[0]),0)#if selection goes off the screen, truncate at 0
+    max_x = max(point1[0],point2[0])
+    min_y = max(min(point1[1],point2[1]),0)
+    max_y = max(point1[1],point2[1])
+    
+    a=(max_x-min_x)//2
+    b=(max_y-min_y)//2
+    h=min_x+a #center x
+    k=min_y+b #center y
+
+    img_h = labels.shape[0]
+    img_w = labels.shape[1]
+
+    Y, X = np.ogrid[:img_h, :img_w]
+    in_ellipse = ( (X-h)**2/a**2 + (Y-k)**2 / b**2 ) <=1
+    np.place(labels, in_ellipse, labelNum)
+
+
 color_map = {
     0: (160,160,160), #gray
     1: (153, 0, 153), #magenta
@@ -23,7 +42,7 @@ color_map = {
     }
 
 labels = np.zeros((500,500),dtype=np.int32)
-circle_select(labels, 4, (500,600), 1000)
+ellipse_select(labels, 4, (100,200), (400,300))
 
 
 rgb = labeled2rgb(labels,color_map)
