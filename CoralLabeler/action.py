1838c22035a358d1a8960e6c13ab3cd661a6a244
Dylan Chapell <dylanchapell@rocketmail.com>
2024-02-06 10:48:27 -0700

    Add parent option that might be used by QML code

--- CoralLabeler/action.py
+++ CoralLabeler/action.py
@@ -30,32 +30,32 @@ class Action(QObject):
 @QmlElement
 class CreateAction(Action) :
     """This action creates a polygon shape with a specified set of points"""
-    def __init__(self, parent=None, target=None, coordinate_array = None):
-        super().__init__(None)
+    def __init__(self,parent = None, shapeParent=None, target=None, coordinate_array = None):
+        super().__init__(parent)
         if coordinate_array is not None:
             print("Creating from arrays in python not supported yet", file=sys.stderr)
         if coordinate_array is None and target is None:
             print("No coordinate array or pre-created object provided", file=sys.stderr)
         self.target = target
-        self.shapeParent = parent
+        self.shapeParent = shapeParent
 
 
 @QmlElement
 class DeleteAction(Action) :
     """This action deletes a specified shape"""
-    def __init__(self,parent=None, target=None):
-        super().__init__(None)
+    def __init__(self,parent=None, shapeParent=None, target=None):
+        super().__init__(parent)
         self.target = target
-        self.shapeParent = parent
+        self.shapeParent = shapeParent
 
 
 @QmlElement
 class MoveAction(Action) :
     """This action moves a shape by specified distances in the x and y directions"""
-    def __init(self,parent=None, target=None, dX=0, dY=0):
-        super().__init__(None)
+    def __init(self,parent = None,shapeParent=None, target=None, dX=0, dY=0):
+        super().__init__(parent)
         self.target = target
-        self.shapeParent = parent
+        self.shapeParent = shapeParent
         self.dX = dX
         self.dY = dY
     
@@ -80,10 +80,10 @@ class ScaleAction(Action):
     """This action operates on shapes that can be scaled, like rectangles and ovals,
         and changes their height and width by the specified amounts
     """
-    def __init__(self,parent=None, target=None,sX=1,sY=1):
-        super().__init__(None)
+    def __init__(self, parent = None, shapeParent=None, target=None,sX=1,sY=1):
+        super().__init__(parent)
         self.target = target
-        self.shapeParent = parent
+        self.shapeParent = shapeParent
         self.sX = sX
         self.sY = sY
 
